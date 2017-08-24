715fccf7abc0958283840c0c0a34eb0e5df20b2e
gagan0183 <g.deepsingh1@gmail.com>
2017-08-25 00:40:53 +0530

    comp battleship js

--- battleshipgame/battleship.js
+++ battleshipgame/battleship.js
@@ -22,5 +22,33 @@ var model = {
         {locations: ["06", "16", "26"], hits: ["", "", ""]},
         {locations: ["24", "34", "44"], hits: ["", "", ""]},
         {locations: ["10", "11", "12"], hits: ["", "", ""]}
-    ]
+    ],
+    fire: function (guess) {
+        for(var i = 0; i < this.numShips; i++) {
+            var ship = this.ships[i];
+            locations = ship.locations;
+            var index = locations.indexOf(guess);
+            if(index >= 0) {
+                ship.hits[index] = "hit";
+                view.displayHit(guess);
+                view.displayMessage("HIT!");
+                if(this.isSunk(ship)) {
+                    view.displayMessage("You sank my battleship");
+                    this.shipSunk++;
+                }
+                return true;
+            }
+        }
+        view.displayMiss(guess);
+        view.displayMessage("You miss");
+        return false;
+    },
+    isSunk: function (shiip) {  
+        for(var i = 0; i < this.numShips; i++) {
+            if(ship.hits[i] !== 'hit') {
+                return false;
+            }
+        }
+        return true;
+    } 
 };
